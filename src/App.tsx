8b69032dd87085863bafc04141f4f3cf39a2bb68
Gaurang <sgaurang@hotmail.com>
2023-10-15 08:43:07 -0400

    converted to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useEffect } from 'react';
-
-import './App.css';
-
-import theme from './themes';
-
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import Layout from './layout';
-import axios from 'axios';
-
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  useEffect(() => {
-    
-    axios.get("https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=I04T80M9FZE3JJC5&datatype=json").then((result)=>{
-      console.log(result);
-    })
-    
-    
-  }, [])
-  
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>        
-        <Layout/>
-      </ThemeProvider>
-      
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
