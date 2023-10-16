a6308a62d2693e1382d312b6812676e6abdfb02f
Gaurang <sgaurang@hotmail.com>
2023-10-16 01:18:20 -0400

    Modified Menu layout

--- src/scenes/global/Sidebar.tsx
+++ src/scenes/global/Sidebar.tsx
@@ -8,6 +8,7 @@ import {
   menuClasses,
 } from "react-pro-sidebar";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import TroubleshootIcon from "@mui/icons-material/Troubleshoot";
 // import "react-pro-sidebar/dist/css/styles.css";
 
 const Sidebar = () => {
@@ -23,30 +24,43 @@ const Sidebar = () => {
         height: "100%",
       }}
     >
-      <SidebarPro collapsed={isCollepsed} backgroundColor={colors.primary[400]}>
+      <SidebarPro
+        rootStyles={{
+          ["." + menuClasses.button]: {
+            "&:hover": {
+              backgroundColor: "transparent !important",
+            },
+          },
+        }}
+        collapsed={isCollepsed}
+        backgroundColor={colors.primary[400]}
+      >
         <Menu>
           <MenuItem
             onClick={() => setIsCollepsed(!isCollepsed)}
-            icon={isCollepsed ? <MenuOutlinedIcon /> : undefined}
+            icon={isCollepsed ? <TroubleshootIcon /> : undefined}
             // icon={<MenuOutlinedIcon />}
-            rootStyles={{
-              ["." + menuClasses.button]: {
-                "&:hover": {
-                  backgroundColor: "transparent !important",
-                },
-              },
-            }}
+
             style={{ margin: "10px 0 20px 0", color: colors.grey[100] }}
           >
             {!isCollepsed && (
-              <Box>
-                <Typography>Vyakar</Typography>
-                <IconButton>
-                  <MenuOutlinedIcon />
-                </IconButton>
+              <Box display="flex" justifyContent="space-between">
+                <Box display="flex">
+                  <TroubleshootIcon />
+                  <Typography>Vyakar</Typography>
+                </Box>
+                <Box display="flex">
+                  <IconButton>
+                    <MenuOutlinedIcon />
+                  </IconButton>
+                </Box>
               </Box>
             )}
           </MenuItem>
+          <MenuItem active>Documentation</MenuItem>
+          <MenuItem> Calendar</MenuItem>
+          <MenuItem> E-commerce</MenuItem>
+          <MenuItem> Examples</MenuItem>
         </Menu>
       </SidebarPro>
     </Box>
