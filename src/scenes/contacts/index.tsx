a1c4118142f82a11ca820b1c948c0b77d3b3f72a
Gaurang <sgaurang@hotmail.com>
2023-10-17 20:50:27 -0400

    Cleaned Code

--- src/scenes/contacts/index.tsx
+++ src/scenes/contacts/index.tsx
@@ -3,9 +3,6 @@ import { DataGrid, GridToolbar, GridColDef } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import { mockDataContacts } from "../../data/mockData";
 
 const Contacts = () => {
